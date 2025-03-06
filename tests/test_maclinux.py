2c76dbc15f96610b66a5f66075436d492990fff1
Michael Smith <pointmatic@gmail.com>
2025-03-06 01:34:02 -0800

    replaced MIT license with Mozilla 2.0

--- tests/test_maclinux.py
+++ tests/test_maclinux.py
@@ -2,7 +2,7 @@
 """
 Copyright (c) 2025 Pointmatic, (https://www.pointmatic.com)
 
-This source code is licensed under the MIT License found in the
+This source code is licensed under the Mozilla Public License Version 2.0 found in the
 LICENSE file in the root directory of this source tree.
 
 ============================================================================================================================================================================================
